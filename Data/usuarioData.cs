95c3339321e4c4c55f11bb6bcc6d6efd41bed13e
luism <marmol>
2021-12-01 12:09:03 -0400

    Meh

---
+++ Data/usuarioData.cs
@@ -0,0 +1,21 @@
+﻿using Entidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class usuarioData
+    {
+        BancoAPIEntities db = new();
+
+        public void insertarUsuario(usuario usuario)
+        {
+            db.usuarios.Add(usuario);
+            db.SaveChanges();
+
+        }
+    }
+}
---
+++ Negocio/usuarionegocio.cs
@@ -0,0 +1,20 @@
+﻿using Data;
+using Entidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class usuarionegocio
+    {
+        usuarioData Usuario = new();
+
+        public void InsertarUsuario(usuario usuario)
+        {
+            Usuario.insertarUsuario(usuario);
+        }
+    }
+}
